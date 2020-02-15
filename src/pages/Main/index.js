a59f1bc5363c79c0e61beac8d2d657d78be84461
José Miranda <viictoroliver@gmail.com>
2020-02-15 02:21:10 -0300

    :rocket:

---
+++ src/pages/Main/index.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { FaGitAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import Container from '../../components/Container/index';
+import { Form, SubmitButton, List } from './styles';
+import Repository from '../Repository';
+
+export default class Main extends Component {
+	state = {
+		newRepo: '',
+		repositories: [],
+		loading: 0,
+	};
+
+	// caregar os dados do localStorage
+	componentDidMount() {
+		const repositories = localStorage.getItem('repositories');
+
+		if (repositories) {
+			this.setState({ repositories: JSON.parse(repositories) });
+		}
+	}
+
+	componentDidUpdate(_, prevState) {
+		const { repositories } = this.state;
+		if (prevState.repositories !== repositories) {
+			localStorage.setItem('repositories', JSON.stringify(repositories));
+		}
+	}
+
+	handleInputChange = e => {
+		this.setState({ newRepo: e.target.value });
+	};
+
+	handleSubmit = async e => {
+		e.preventDefault();
+
+		this.setState({ loading: 1 });
+
+		const { newRepo, repositories } = this.state;
+
+		const response = await api.get(`/repos/${newRepo}`);
+
+		const data = {
+			name: response.data.full_name,
+		};
+
+		this.setState({
+			repositories: [...repositories, data],
+			newRepo: '',
+			loading: 0,
+		});
+	};
+
+	render() {
+		const { newRepo, loading, repositories } = this.state;
+		return (
+			<Container>
+				<h1>
+					<FaGitAlt />
+					Repositórios
+				</h1>
+
+				<Form onSubmit={this.handleSubmit}>
+					<input
+						type="text"
+						placeholder="Adicionar repositório"
+						value={newRepo}
+						onChange={this.handleInputChange}
+					/>
+
+					<SubmitButton loading={this.state.loading ? 1 : 0}>
+						{loading ? (
+							<FaSpinner color="#fff" size={14} />
+						) : (
+							<FaPlus color="#fff" size={14} />
+						)}
+					</SubmitButton>
+				</Form>
+
+				<List>
+					{repositories.map(repository => (
+						<li key={repository.name}>
+							<span>{repository.name}</span>
+							<Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+								Detalhes
+							</Link>
+						</li>
+					))}
+				</List>
+			</Container>
+		);
+	}
+}
